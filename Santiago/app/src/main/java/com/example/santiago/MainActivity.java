d3fc08a66822d979df68794f7b2b10d47ed77c58
Walterwcms <83028154+Walterwcms@users.noreply.github.com>
2021-11-26 10:46:23 -0100

    Add files via upload

---
+++ Santiago/app/src/main/java/com/example/santiago/MainActivity.java
@@ -0,0 +1,22 @@
+/** Autor: Kleiton Da Rosa
+ *         Vandinelson Gomes
+ *         Walter Dos Santos
+ *
+ */
+
+
+package com.example.santiago;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
